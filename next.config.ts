14619738bfc5f733b1d1f056f1b91176fbb14474
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-04-15 23:56:56 +0200

    feat(config): add remote image pattern for kappa.lol to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'kappa.lol',
+				port: '',
+				pathname: '**',
+				search: '',
+			},
+		],
+	},
 };
 
 export default nextConfig;
