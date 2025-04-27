fedf7c84bb0550e35f5add4c6a2e67f26c80bfa1
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-04-27 23:12:12 +0200

    feat(home): enhance About Me section with links and button for My Links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import { Clock } from '@/components/clock';
 import { ProjectCard } from '@/components/project-card';
 import { ThemeToggle } from '@/components/theme-toggle';
+import { Button } from '@/components/ui/button';
 import { projects } from '@/lib/data';
+import Link from 'next/link';
 
 export default function Home() {
 	return (
@@ -12,7 +14,7 @@ export default function Home() {
 
 			<header className='pt-16 pb-8 px-6 md:px-12 lg:px-24'>
 				<div className='flex items-center gap-4'>
-					<h1 className='text-2xl md:text-3xl font-bold'>
+					<h1 className='text-2xl md:text-3xl font-bold select-none'>
 						prodbyeagle
 					</h1>
 					<div className='font-mono items-baseline text-lg'>
@@ -23,19 +25,53 @@ export default function Home() {
 
 			<main className='px-6 md:px-12 lg:px-24 pb-24'>
 				<section className='mb-16'>
-					<h2 className='text-xl font-medium mb-6 border-b pb-2'>
+					<h2 className='text-xl font-medium mb-6 border-b pb-2 select-none'>
 						About Me
 					</h2>
-					<p className='max-w-2xl'>
-						I&apos;m a passionate developer focused on modern web
-						technologies, specializing in React, TypeScript, and
-						Next.js to create performant and user-friendly web
-						applications.
-					</p>
+					<div className='max-w-2xl space-y-4'>
+						<p>
+							I&apos;m a passionate developer focused on modern
+							web technologies, specializing in{' '}
+							<Link
+								href='https://react.dev'
+								target='_blank'
+								rel='noopener noreferrer'
+								className='text-[#61dafb]/80 hover:underline underline-offset-2'>
+								React
+							</Link>
+							,{' '}
+							<Link
+								href='https://www.typescriptlang.org/'
+								target='_blank'
+								rel='noopener noreferrer'
+								className='text-[#3178c6]/80 hover:underline underline-offset-2'>
+								TypeScript
+							</Link>{' '}
+							and{' '}
+							<Link
+								href='https://nextjs.org/'
+								target='_blank'
+								rel='noopener noreferrer'
+								className='text-[#f0f0f0] hover:underline underline-offset-2'>
+								Next.js
+							</Link>{' '}
+							to create performant and user-friendly web
+							applications.
+						</p>
+						<Link
+							href='https://eaglelink.vercel.app/prodbyeagle'
+							target='_blank'
+							rel='noopener noreferrer'
+							aria-label='Visit my EagleLink profile'>
+							<Button variant='outline' size='sm'>
+								My Links
+							</Button>
+						</Link>
+					</div>
 				</section>
 
 				<section className='mb-16'>
-					<h2 className='text-xl font-medium mb-6 border-b pb-2'>
+					<h2 className='text-xl font-medium mb-6 border-b pb-2 select-none'>
 						Newest Projects
 					</h2>
 					<div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
