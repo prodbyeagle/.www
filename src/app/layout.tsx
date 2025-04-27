95d8b4b8a528ef09c03da986127fb1915406a31d
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-04-27 22:31:14 +0200

    refactor(layout): remove Geist font integration and simplify font classes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from 'next';
-import { Geist_Mono, Geist } from 'next/font/google';
+import { Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from '@/components/theme-provider';
 import { Footer } from '@/components/footer';
 import { DottedBackground } from '@/components/dotted-background';
 import { ReactScan } from '@/components/react-scan';
-
-const geistSans = Geist({
-	variable: '--font-geist-sans',
-	subsets: ['latin'],
-});
 
 const geistMono = Geist_Mono({
 	variable: '--font-geist-mono',
@@ -30,7 +25,7 @@ export default function RootLayout({
 	return (
 		<html lang='en' suppressHydrationWarning>
 			<body
-				className={`${geistMono.variable} ${geistSans.variable} antialiased font-sans tracking-tight cursor-default select-none`}>
+				className={`${geistMono.variable} antialiased font-mono tracking-tight cursor-default select-none`}>
 				<ThemeProvider
 					attribute='class'
 					defaultTheme='system'
