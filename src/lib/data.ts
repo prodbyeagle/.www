9d048991314fcf1b6a2394b9d870a76c07734305
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-04-27 22:15:01 +0200

    feat(data): add new projects EagleChat and MeowRecorder with descriptions and links

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -11,6 +11,25 @@ export interface Project {
 
 export const projects: Project[] = [
 	{
+		id: 'eaglechat',
+		title: 'EagleChat',
+		description: 'An Chat Overlay for OBS Studio.',
+		longDescription:
+			'EagleChat is a chat overlay for OBS Studio that allows streamers to display their Twitch chat on their stream.',
+		tags: ['TypeScript'],
+		imageUrl: 'https://kappa.lol/WzWZw0',
+		github: '	https://github.com/prodbyeagle/eaglechat',
+	},
+	{
+		id: 'meowrecorder',
+		title: 'MeowRecorder',
+		description: 'An Discord bot that records voice channels.',
+		longDescription:
+			'EagleRecorder is a Discord bot that allows users to record voice channels and save the recordings for later use. It is designed to be easy to use and lightweight, making it a great addition to any Discord server.',
+		tags: ['TypeScript'],
+		github: 'https://github.com/prodbyeagle/MeowRecorder',
+	},
+	{
 		id: 'eaglelink',
 		title: 'EagleLink',
 		description: 'A small website for links.',
