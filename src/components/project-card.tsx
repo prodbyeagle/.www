d42a0701f17903e3457cae8d6163a76e64c827b2
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-05-07 01:30:31 +0200

    description smaller

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -14,7 +14,7 @@ export function ProjectCard({ project }: ProjectCardProps) {
 				<h3 className='text-2xl font-semibold text-foreground group-hover:text-primary transition-colors'>
 					<WordReveal text={project.projectTitle} />
 				</h3>
-				<p className='text-md text-muted-foreground line-clamp-3'>
+				<p className='text-base text-muted-foreground line-clamp-3'>
 					<WordReveal text={project.projectDescription} />
 				</p>
 
