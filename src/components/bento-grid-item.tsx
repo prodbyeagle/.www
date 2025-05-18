e02bb0870c44b778b6a43e1f9ce14ab1d293830b
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-05-19 00:07:40 +0200

    removed project code

--- src/components/bento-grid-item.tsx
+++ src/components/bento-grid-item.tsx
@@ -1,84 +1,18 @@
-'use client';
+import { type ReactNode } from 'react';
 
-import confetti from 'canvas-confetti';
-import { useEffect, useState, type ReactNode } from 'react';
-
-import { IProject } from '@/lib/projects';
 import { cn } from '@/lib/utils';
 
 interface BentoGridItemProps {
 	className?: string;
-	projectData: IProject;
 	children: ReactNode;
 }
 
-const getBackgroundClass = (type?: IProject['projectType']) => {
-	switch (type) {
-		case 'inactive':
-			return 'hover:border-amber-300/20 not-dark:hover:bg-amber-300/5';
-		case 'discontinued':
-			return 'hover:border-destructive/20 not-dark:hover:bg-destructive/5';
-		case 'active':
-		default:
-			return 'bg-background dark:hover:border-accent';
-	}
-};
-
-export function BentoGridItem({
-	className,
-	children,
-	projectData,
-}: BentoGridItemProps) {
-	const backgroundClass = getBackgroundClass(projectData.projectType);
-
-	const [isHighlighted, setIsHighlighted] = useState(false);
-
-	useEffect(() => {
-		if (!projectData.isNew) return;
-
-		setIsHighlighted(true);
-
-		const timeoutId = setTimeout(() => {
-			setIsHighlighted(false);
-		}, 2500);
-
-		const end = Date.now() + 2000;
-		const colors = ['#a786ff', '#fd8bbc', '#eca184', '#f8deb1'];
-
-		const frame = () => {
-			if (Date.now() > end) return;
-
-			confetti({
-				particleCount: 2,
-				angle: 60,
-				spread: 55,
-				startVelocity: 60,
-				origin: { x: 0, y: 0.5 },
-				colors,
-			});
-			confetti({
-				particleCount: 2,
-				angle: 120,
-				spread: 55,
-				startVelocity: 60,
-				origin: { x: 1, y: 0.5 },
-				colors,
-			});
-
-			requestAnimationFrame(frame);
-		};
-
-		frame();
-
-		return () => clearTimeout(timeoutId);
-	}, [projectData.isNew]);
-
+export function BentoGridItem({ className, children }: BentoGridItemProps) {
 	return (
 		<div
 			className={cn(
-				'rounded-none border border-border/70 backdrop-blur-sm p-6 transition-all duration-300 not-dark:hover:shadow-md',
-				backgroundClass,
-				isHighlighted && 'rounded-sm border-primary/50 border-2',
+				'rounded-none border bg-background dark:hover:border-accent border-border/70 backdrop-blur-sm p-6 transition-all duration-300 not-dark:hover:shadow-md',
+
 				className
 			)}>
 			{children}
