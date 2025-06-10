1545fd8f2beee9ca46a8be5e36d52ff4262c98ef
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-06-11 00:21:30 +0200

    refactor: update ProjectCard to use Project type and standardize property names

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -1,13 +1,12 @@
+import { Project } from '@/types';
 import { ExternalLink, Github } from 'lucide-react';
 import Link from 'next/link';
-
-import type { IProject } from '@/lib/projects';
 
 import { Badge } from './ui/badge';
 import { WordReveal } from './word-reveal';
 
 interface ProjectCardProps {
-	project: IProject;
+	project: Project;
 }
 
 export function ProjectCard({ project }: ProjectCardProps) {
@@ -15,15 +14,15 @@ export function ProjectCard({ project }: ProjectCardProps) {
 		<div className='group relative cursor-default flex flex-col justify-between h-full shadow-input transition-all duration-200 overflow-hidden rounded-md'>
 			<div className='flex flex-col gap-1 p-4'>
 				<h3 className='text-2xl font-semibold'>
-					<WordReveal text={project.projectTitle} />
+					<WordReveal text={project.title} />
 				</h3>
 				<p className='text-base text-text-secondary line-clamp-3'>
-					<WordReveal text={project.projectDescription} />
+					<WordReveal text={project.description} />
 				</p>
 
-				{project.projectTags?.length > 0 && (
+				{project.tags?.length > 0 && (
 					<div className='flex flex-wrap gap-1 mt-3'>
-						{project.projectTags.slice(0, 5).map((tag) => (
+						{project.tags.slice(0, 5).map((tag) => (
 							<Badge variant='outline' key={tag}>
 								{tag}
 							</Badge>
@@ -32,7 +31,7 @@ export function ProjectCard({ project }: ProjectCardProps) {
 				)}
 			</div>
 
-			{(project.githubUrl || project.projectUrl) && (
+			{(project.githubUrl || project.url) && (
 				<div className='absolute top-2 right-2 flex gap-2 opacity-0 group-hover:opacity-100 transition-opacity duration-200'>
 					{project.githubUrl && (
 						<Link
@@ -43,9 +42,9 @@ export function ProjectCard({ project }: ProjectCardProps) {
 							<Github className='size-4 duration-200 text-muted-foreground hover:text-foreground' />
 						</Link>
 					)}
-					{project.projectUrl && (
+					{project.url && (
 						<Link
-							href={project.projectUrl}
+							href={project.url}
 							target='_blank'
 							rel='noopener noreferrer'
 							aria-label='External Link'>
