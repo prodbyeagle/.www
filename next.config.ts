4976eedfb62d1da5d2f1aec2c20afe967e10b8de
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-06-15 01:30:40 +0200

    feat: add async redirects for GitHub, Discord, and social links

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,25 @@ const nextConfig: NextConfig = {
 			},
 		],
 	},
+	async redirects() {
+		return [
+			{
+				source: '/github',
+				destination: 'https://github.com/prodbyeagle',
+				permanent: true,
+			},
+			{
+				source: '/discord',
+				destination: 'https://discord.gg/KNc4bnA2fU',
+				permanent: true,
+			},
+			{
+				source: '/socials',
+				destination: 'https://eaglelink.vercel.app/prodbyeagle',
+				permanent: true,
+			},
+		];
+	},
 };
 
 export default nextConfig;
