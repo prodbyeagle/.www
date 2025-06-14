062dc00325cdd8b91a902b8218e2dc99ea7be42a
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-06-15 01:59:41 +0200

    refactor: enhance className usage with cn utility for better readability

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -2,6 +2,8 @@ import { Project } from '@/types';
 import { ExternalLink, Github } from 'lucide-react';
 import Link from 'next/link';
 
+import { cn } from '@/lib/utils';
+
 import { Badge } from './ui/badge';
 import { WordReveal } from './word-reveal';
 
@@ -11,7 +13,11 @@ interface ProjectCardProps {
 
 export function ProjectCard({ project }: ProjectCardProps) {
 	return (
-		<div className='group relative cursor-default flex flex-col justify-between h-full shadow-input transition-all duration-200 overflow-hidden rounded-md'>
+		<div
+			className={cn(
+				'group relative cursor-default flex flex-col justify-between h-full',
+				'shadow-input transition-all duration-200 overflow-hidden rounded-md'
+			)}>
 			<div className='flex flex-col gap-1 p-4'>
 				<h3 className='text-2xl font-semibold'>
 					<WordReveal text={project.title} />
@@ -19,27 +25,39 @@ export function ProjectCard({ project }: ProjectCardProps) {
 				<p className='text-base text-text-secondary line-clamp-3'>
 					<WordReveal text={project.description} />
 				</p>
-
 				{project.tags?.length > 0 && (
 					<div className='flex flex-wrap gap-1 mt-3'>
 						{project.tags.slice(0, 5).map((tag) => (
-							<Badge variant='outline' key={tag}>
+							<Badge
+								key={tag}
+								variant='outline'
+								className={cn(
+									tag.toLowerCase() === 'deprecated' &&
+										'border-red-500/10'
+								)}>
 								{tag}
 							</Badge>
 						))}
 					</div>
 				)}
 			</div>
-
 			{(project.githubUrl || project.url) && (
-				<div className='absolute top-2 right-2 flex gap-2 opacity-0 group-hover:opacity-100 transition-opacity duration-200'>
+				<div
+					className={cn(
+						'absolute top-2 right-2 flex gap-2',
+						'opacity-0 group-hover:opacity-100 transition-opacity duration-200'
+					)}>
 					{project.githubUrl && (
 						<Link
 							href={project.githubUrl}
 							target='_blank'
 							rel='noopener noreferrer'
 							aria-label='GitHub'>
-							<Github className='size-4 duration-200 text-muted-foreground hover:text-foreground' />
+							<Github
+								className={cn(
+									'size-4 duration-200 text-muted-foreground hover:text-foreground'
+								)}
+							/>
 						</Link>
 					)}
 					{project.url && (
@@ -48,7 +66,11 @@ export function ProjectCard({ project }: ProjectCardProps) {
 							target='_blank'
 							rel='noopener noreferrer'
 							aria-label='External Link'>
-							<ExternalLink className='size-4 duration-200 text-muted-foreground hover:text-foreground' />
+							<ExternalLink
+								className={cn(
+									'size-4 duration-200 text-muted-foreground hover:text-foreground'
+								)}
+							/>
 						</Link>
 					)}
 				</div>
